57dbba8c842e69b07c4c15e1510c646b0022cf72
pkonieczny <it.patryk.konieczny@gmail.com>
2024-05-10 11:31:42 +0200

    Initial commit

---
+++ src/main/java/pl/lunchclub/LunchClubApplication.java
@@ -0,0 +1,13 @@
+package pl.lunchclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lunchclub/LunchClubApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lunchclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
